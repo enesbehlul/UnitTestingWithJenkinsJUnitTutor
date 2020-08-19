17c4bcb88917db8fd444a74fdb53dba84abd6fc6
unknown <ebehlul.yenidunya@stu.fsm.edu.tr>
2020-08-19 13:36:08 +0300

    firs commit

---
+++ test/unit_test_example/TestMatematik.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unit_test_example;
+
+import matematik.Matematik;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author behlul
+ */
+public class TestMatematik {
+
+    Matematik mat;
+
+    public TestMatematik() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        mat = new Matematik(7, 10);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, mat.add());
+    }
+}
